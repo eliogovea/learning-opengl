fdb17defdfc5fa5dd34261cd5d664a8defe14a09
Elio Govea <eliogovea1993@gmail.com>
2019-10-26 13:21:03 -0400

    fix typo: teture location

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,13 @@
 #include "object.hpp"
 
 namespace objects {
-  static std::vector<vertex> vertices_1 = {
-    {{0.0f, 0.0f, 0.0f}},
-    {{0.5f, 0.0f, 0.0f}},
-    {{0.0f, 0.5f, 0.0f}}
+  std::vector<vertex> vertices_1 = {
+    {{0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}},
+    {{0.5f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}},
+    {{0.0f, 0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}}
   };
 
-  static object triangle_1{vertices_1, std::vector<GLuint>{0, 1, 2}};
+  object triangle_1{vertices_1, std::vector<GLuint>{0, 1, 2}};
 
   static std::vector<vertex> vertices_pyramid = {
     {{0.0f, 0.0f, 0.0f}},
@@ -88,35 +88,45 @@ int main(int argc, char* argv[]) {
   glm::mat4 model{1.0f};
 
   glm::mat4 view = glm::lookAt(glm::vec3(0, 0, -5), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-
   glm::mat4 proj = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
 
-  // objects::triangle_1.init();
+  shader_.bind();
+  glUniformMatrix4fv(shader_.locate_uniform("view"), 1, GL_FALSE, &view[0][0]);
+  glUniformMatrix4fv(shader_.locate_uniform("proj"), 1, GL_FALSE, &proj[0][0]);
+
   objects::cube.init(); // !!!
+  objects::triangle_1.init();
   
   // main loop
 	while (!window.is_closed()) {
     window.clear();
 
-    shader_.bind();
-
     step++;
 
     float v = 2.0 * sin((float)step / 100.0);
 
+    // pyramid
     model = glm::mat4(1.0f);
     model = glm::translate(glm::vec3(-0.5, -0.5, 0.0)) * model;
     model = glm::scale(glm::vec3(0.5, 0.5, 0.5))       * model;
     model = glm::translate(glm::vec3(v, v, 0))         * model;
 
+    shader_.bind();
     glUniformMatrix4fv(shader_.locate_uniform("model"), 1, GL_FALSE, &model[0][0]);
-    glUniformMatrix4fv(shader_.locate_uniform("view"), 1, GL_FALSE, &view[0][0]);
-    glUniformMatrix4fv(shader_.locate_uniform("proj"), 1, GL_FALSE, &proj[0][0]);
 
-    // objects::triangle_1.render();
-    // objects::pyramid.render();
     objects::cube.render();
-    
+
+    // triangle_1
+    model = glm::mat4(1.0f);
+    model = glm::translate(glm::vec3(-0.5, -0.5, 0.0)) * model;
+    model = glm::scale(glm::vec3(0.5, 0.5, 0.5))       * model;
+    model = glm::translate(glm::vec3(v, -v, 0))        * model;
+
+    shader_.bind();
+    glUniformMatrix4fv(shader_.locate_uniform("model"), 1, GL_FALSE, &model[0][0]);
+
+    objects::triangle_1.render();
+
     window.update();
 	}
 
--- src/object.cpp
+++ src/object.cpp
@@ -39,6 +39,16 @@ void object::init() {
   glBindVertexArray(va_);
     glBindBuffer(GL_ARRAY_BUFFER, vb_);
     glBufferData(GL_ARRAY_BUFFER, vertices_.size() * sizeof(vertex), &vertices_[0], GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (GLvoid*)0);
+    glEnableVertexAttribArray(0); // position
+    
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (GLvoid*)(3 * sizeof (GLfloat)));
+    glEnableVertexAttribArray(1); // color
+    
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (GLvoid*)(6 * sizeof (GLfloat)));
+    glEnableVertexAttribArray(2); // texture
+    
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eb_);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_.size() * sizeof(GLuint), &indices_[0], GL_STATIC_DRAW);
   glBindVertexArray(0);
@@ -46,15 +56,7 @@ void object::init() {
 
 void object::render() {
   glBindVertexArray(va_);
-    glEnableVertexAttribArray(0); // position
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (GLvoid*)0);
-    glEnableVertexAttribArray(1); // color
-      glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof (vertex), (GLvoid*)(3 * sizeof (GLfloat)));
-    glEnableVertexAttribArray(2); // texture
-      glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof (vertex), (GLvoid*)(6 * sizeof (GLfloat)));
     glDrawElements(GL_TRIANGLES, indices_.size(), GL_UNSIGNED_INT, 0);
-	  glDisableVertexAttribArray(0);
-	  glDisableVertexAttribArray(1);
   glBindVertexArray(0);
 }
 
